6b8e80c169d442d55874994936ce3b40bd2f77b3
Kyle Bittinger <kylebittinger@gmail.com>
2019-04-25 16:00:09 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='dnabc',
-    version="0.0.4",
+    version="0.0.5",
     description='Demultiplex pooled DNA sequencing data',
     author='Kyle Bittinger',
     author_email='kylebittinger@gmail.com',
